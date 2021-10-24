4662cf0ef0aabf7c7f1d39b167d3de54b46ab817
LechugaTactica <79500304+LechugaTactica@users.noreply.github.com>
2021-10-24 17:01:18 -0300

    Update views.py

--- pokeapi/usuarios/views.py
+++ pokeapi/usuarios/views.py
@@ -18,12 +18,14 @@ def login(request):
 
 def register(request):
     if request.method == "POST":
-        user = request.POST.get("user")
+        user = request.POST.get("username")
+        name = request.POST.get("first_name")
+        nickname = request.POST.get("last_name")
         password = request.POST.get("password")
         try:
-            user = User.objects.get(username=user, password=password)
+            user = User.objects.get(username=user, password=password, first_name=name, last_name=nickname)
         except:
-            user = User.objects.create_user(username=user, password=password)
+            user = User.objects.create_user(username=user, password=password, first_name=name, last_name=nickname)
             return redirect("menu")       
     return render(request, "register.html")
 
