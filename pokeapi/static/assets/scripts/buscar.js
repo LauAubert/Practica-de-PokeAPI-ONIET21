7ab7ade5cca9ded87856bd3e166640fe05de9aaa
LauAubert <aubertlautaroa@gmail.com>
2021-10-24 18:35:09 -0300

    fix buscar.js

--- pokeapi/static/assets/scripts/buscar.js
+++ pokeapi/static/assets/scripts/buscar.js
@@ -1,7 +1,8 @@
 const searchbar = document.getElementById('pokenames')
-
+const pokeid = document.getElementById('pokeid').innerHTML
 searchbar.addEventListener('click',function(){
     // window.location.href = "http://www.w3schools.com";
     var url = window.location.origin
-    window.location.href = url + "/menu/"+searchbar.value
+    if (searchbar.value != pokeid && searchbar.value!=0 ){
+        window.location.href = url + "/menu/"+searchbar.value}
 })
