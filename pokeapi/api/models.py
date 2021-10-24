4b95708d7bd55a07e37c95f4de9e7a26b024a405
LechugaTactica <79500304+LechugaTactica@users.noreply.github.com>
2021-10-24 16:27:20 -0300

    Update models.py

--- pokeapi/api/models.py
+++ pokeapi/api/models.py
@@ -5,4 +5,4 @@ from django.db.models.fields import CharField, IntegerField
 
 class Favoritos(models.Model):
     user = object(max_length=50)
-    pok_id = IntegerField(max_length=50)
+    pokeid = IntegerField(max_length=50)
