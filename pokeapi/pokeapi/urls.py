dbbefa5360a56811810cf23b5e5f1ebdaf8cfb6c
Juanmakrum <juanmibarreto12@gmail.com>
2021-10-24 15:27:44 -0300

    Update urls.py

--- pokeapi/pokeapi/urls.py
+++ pokeapi/pokeapi/urls.py
@@ -19,6 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('usuarios/', include('usuarios.urls')),
+    path('usuarios/', include('usuarios.urls')),
     path('', include('api.urls'))
 ]
