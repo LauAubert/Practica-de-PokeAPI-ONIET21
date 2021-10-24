314d19894f192bd9902ffd1733db9729d0be8180
LechugaTactica <79500304+LechugaTactica@users.noreply.github.com>
2021-10-24 15:27:58 -0300

    Update views.py

--- pokeapi/usuarios/views.py
+++ pokeapi/usuarios/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 # Create your views here.
-def prueba(request):
-    return render(request, "prueba.html")
+
+def login(request):
+    if request.method == "POST":
+        if request.POST.get("submit") == "submit":
+            user = request.POST.get("user")
+            password = request.POST.get("password")
+
+        if request.POST.get("register") == "register":
+            return redirect("register")    
+        
+    return render(request, "login.html")
+
+
+def register(request):
+    
+    User.create_user(username=user, password=password)
+            
+    return render(request, "register.html")
+
+
+def logout(request):
+    
+    return render(request, "login.html")
+
