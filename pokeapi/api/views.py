b76ce18cbf116f892c7219af830d2b2930aaa742
Juanmakrum <juanmibarreto12@gmail.com>
2021-10-24 15:24:19 -0300

    prueba

--- pokeapi/api/views.py
+++ pokeapi/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+import json,requests
+from django.contrib.auth.models import User
 # Create your views here.
+
+def prueba(request):
+    return render(request, 'prueba.html')
+
+
