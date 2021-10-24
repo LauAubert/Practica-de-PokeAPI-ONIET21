1e8a1b2cd540e83e8c0e0c2f4ecae0ded70dd626
LechugaTactica <79500304+LechugaTactica@users.noreply.github.com>
2021-10-24 17:33:13 -0300

    Update views.py

--- pokeapi/usuarios/views.py
+++ pokeapi/usuarios/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate
 
 # Create your views here.
 
-def login(request):
+def login_auth(request):
     if request.method == "POST":
         user = request.POST.get("username")
         password = request.POST.get("password")
@@ -22,14 +22,15 @@ def register(request):
         name = request.POST.get("first_name")
         nickname = request.POST.get("last_name")
         password = request.POST.get("password")
+        mail = request.POST.get("email")
         try:
-            user = User.objects.get(username=user, password=password, first_name=name, last_name=nickname)
-        except:
-            user = User.objects.create_user(username=user, password=password, first_name=name, last_name=nickname)
-            return redirect("menu")       
+            user = User.objects.create_user(username=user, password=password, first_name=name, last_name=nickname, email=mail)
+            return redirect("menu",0) 
+        except Exception:
+            return render(request, "register.html", {"failed": True})
     return render(request, "register.html")
 
 
-def logout(request):
+def logout_auth(request):
     return redirect(request, "menu")
 
