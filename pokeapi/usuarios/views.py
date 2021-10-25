81dcc9b3c7a4b77571241cbbe4bc4d6d8e44f4db
LechugaTactica <79500304+LechugaTactica@users.noreply.github.com>
2021-10-24 23:18:29 -0300

    Update views.py

--- pokeapi/usuarios/views.py
+++ pokeapi/usuarios/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
 
 def login_auth(request):
     if request.method == "POST":
@@ -10,10 +11,12 @@ def login_auth(request):
         password = request.POST.get("password")
         user = authenticate(username=user, password=password)
         if user is not None:
-            return redirect("menu")
+            login(request=request, user=user)
+            return redirect("menu", 0)
         else:
             return render(request, "login.html", {"Failed": True})  
     return render(request, "login.html")
+
 
 
 def register(request):
@@ -25,12 +28,17 @@ def register(request):
         mail = request.POST.get("email")
         try:
             user = User.objects.create_user(username=user, password=password, first_name=name, last_name=nickname, email=mail)
+            login(request=request, user=user)
             return redirect("menu",0) 
         except Exception:
             return render(request, "register.html", {"failed": True})
     return render(request, "register.html")
 
 
+
+
+@login_required()
 def logout_auth(request):
-    return redirect(request, "menu")
+    logout(request)
+    return redirect("menu", 0)
 
